1359affc13608876634cc79f207e48a4275b48f9
Ron Kurr <kurr@kurron.org>
2015-09-04 20:38:02 +0000

    Got rid of the database dependencies.

--- src/main/groovy/org/kurron/example/rest/outbound/RedisOutboundGateway.groovy
+++ src/main/groovy/org/kurron/example/rest/outbound/RedisOutboundGateway.groovy
@@ -15,14 +15,8 @@
  */
 package org.kurron.example.rest.outbound
 
-import static org.kurron.example.rest.feedback.ExampleFeedbackContext.REDIS_RESOURCE_NOT_FOUND
 import static org.kurron.example.rest.feedback.ExampleFeedbackContext.REDIS_RETRIEVE_INFO
-import static org.kurron.example.rest.feedback.ExampleFeedbackContext.REDIS_STORE_INFO
 import org.kurron.feedback.AbstractFeedbackAware
-import org.kurron.feedback.exceptions.NotFoundError
-import java.util.concurrent.TimeUnit
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.redis.core.RedisOperations
 import org.kurron.stereotype.OutboundGateway
 
 /**
@@ -30,11 +24,6 @@ import org.kurron.stereotype.OutboundGateway
  */
 @OutboundGateway
 class RedisOutboundGateway extends AbstractFeedbackAware implements PersistenceOutboundGateway {
-
-    /**
-     * Handles Redis interactions.
-     */
-    private final RedisOperations<UUID, byte[]> redisOperations
 
     /**
      * The key to use when storing and retrieving the resource's content type.
@@ -46,28 +35,14 @@ class RedisOutboundGateway extends AbstractFeedbackAware implements PersistenceO
      */
     private static final String PAYLOAD_KEY = 'payload'
 
-    @Autowired
-    RedisOutboundGateway( final RedisOperations redisTemplate ) {
-        redisOperations = redisTemplate // WARNING: bean name must be 'redisTemplate' or injection fails
-    }
-
     @Override
     UUID store( final RedisResource resource, final long expirationSeconds ) {
-        def generatedId = UUID.randomUUID()
-        feedbackProvider.sendFeedback( REDIS_STORE_INFO, resource.payload.length, resource.contentType, expirationSeconds, generatedId )
-        redisOperations.opsForHash().putAll( generatedId, [(CONTENT_TYPE_KEY): resource.contentType, (PAYLOAD_KEY): resource.payload] )
-        redisOperations.expire( generatedId, expirationSeconds, TimeUnit.SECONDS )
-        generatedId
+        UUID.randomUUID()
     }
 
     @Override
     RedisResource retrieve( final UUID id ) {
         feedbackProvider.sendFeedback( REDIS_RETRIEVE_INFO, id )
-        def entries = redisOperations.opsForHash().entries( id )
-        if ( !entries ) {
-            feedbackProvider.sendFeedback( REDIS_RESOURCE_NOT_FOUND, id )
-            throw new NotFoundError( REDIS_RESOURCE_NOT_FOUND, id )
-        }
-        new RedisResource( payload: entries[PAYLOAD_KEY] as byte[], contentType: entries[CONTENT_TYPE_KEY] as String )
+        new RedisResource( payload: entries[PAYLOAD_KEY] as byte[], contentType: 'application/json;ronbo=true' )
     }
 }
