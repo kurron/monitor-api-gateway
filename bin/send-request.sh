4724c8fe5ba7b9458f3a1f117229edb5f119781a
Ron Kurr <kurr@kurron.org>
2015-09-10 21:00:52 +0000

    Generate a UUID with each request.

--- bin/send-request.sh
+++ bin/send-request.sh
@@ -2,6 +2,7 @@
 
 for i in `seq 1 100`;
 do
-    curl --verbose --data-ascii @bin/request.json --header "X-Correlation-Id: ${i}" --header 'Content-Type: application/json' localhost:8000
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    curl --verbose --data-ascii @bin/request.json --header "X-Correlation-Id: ${UUID}" --header 'Content-Type: application/json' localhost:8000
 done
 
